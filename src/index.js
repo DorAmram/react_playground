dd6de46c3710fbe98cd3d83eb8af594fa8f786ec
doramram <doramram210@gmail.com>
2020-10-02 10:46:31 +0300

    creating first elements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+
+class Board extends React.Component {
+  renderSquare(i){
+    return <Square value={i} />
+  }
+}
+
+class Square extends React.Component {
+  render () {
+    return 
+    <button className="Square">
+      {this.props.value}
+    </button>
+  }
+}
+
+// ####################################################
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
